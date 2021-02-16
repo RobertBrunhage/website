4599d0318e9c69783500cc610d3e64d894158ac6
John Hilding <johnnhilding@gmail.com>
2021-02-16 15:55:10 +0100

    Add section four

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,6 +96,30 @@ const home = ({ videos }) => {
               </div>
             </div>
           </div>
+          <div className={styles.section_four}>
+            <div className={`max_width ${styles.content}`}>
+              <img className={styles.sprite} src="/assets/icons/sprite_nervous.png" alt="sprite nervous" />
+              <h3 className={styles.title}>
+                Courses coming <span>soon</span>
+              </h3>
+              <p className={styles.desc}>
+                They will be available here. Sign up now to be notified when they are released 😃
+              </p>
+              <form
+                action="https://gmail.us2.list-manage.com/subscribe/post?u=ff73d806dd2f49da87ede8337&amp;id=ed4e712aca"
+                method="post"
+                name="mc-embedded-subscribe-form"
+                target="_blank"
+                noValidate
+              >
+                <input id="email" type="email" placeholder="johndoe@email.com" name="EMAIL" required />
+                <input id="submit" type="submit" value="Notify me" name="subscribe" />
+              </form>
+              <p className={styles.giveaway}>
+                When you sign up you are eligible to have a chance to win future courses
+              </p>
+            </div>
+          </div>
         </div>
       </Layout>
     </PlausibleProvider>
