334e804a0186ad1939f157a0bd98eb132778686e
Robert Brunhage <robertbrunhage@gmail.com>
2021-06-21 11:11:02 +0000

    fix: discord pointing to old server

--- components/footer/socials.ts
+++ components/footer/socials.ts
@@ -6,7 +6,7 @@ interface Social {
 
 const Socials: Social[] = [
   { img: "/assets/icons/twitter.png", href: "https://twitter.com/RobertBrunhage", alt: "twitter" },
-  { img: "/assets/icons/discord.png", href: "https://discord.gg/guJ2Q4D", alt: "discord" },
+  { img: "/assets/icons/discord.png", href: "https://discord.gg/CPwSezC", alt: "discord" },
   { img: "/assets/icons/youtube.png", href: "https://youtube.com/robertbrunhage", alt: "youtube" },
   { img: "/assets/icons/patreon.png", href: "https://www.patreon.com/RobertBrunhage", alt: "patreon" },
 ];
