66e17bcb1a5fd9e74c67118885ed8534b6d93e51
Robert Brunhage <robertbrunhage@gmail.com>
2021-08-24 14:59:13 +0000

    Update FAQ

--- components/faq/course_faq.ts
+++ components/faq/course_faq.ts
@@ -14,19 +14,14 @@ const NavItems: FAQ[] = [
     answer:
       "I will use the latest version of Flutter so in that term we will use Null safety, but I won't specifcally teach about it.",
   },
-  { question: "Will the source code be included?", answer: "Yes 😎" },
-  {
-    question: "Will you have a completion certificate?",
-    answer: "Haven't thought about it, reply on the email with what you think!",
+  { 
+    question: "Will the source code be included?", 
+    answer: "Yes 😎" 
   },
   {
     question: "What will it be priced?",
     answer:
       "I don't know yet. I want to complete the first module with as high production quality as I can. I will set a price depending on how good I can deliver the value to you.",
-  },
-  {
-    question: "How long is the course?",
-    answer: "I don't know yet, but I expect around 5h or more.",
   },
 ];
 
