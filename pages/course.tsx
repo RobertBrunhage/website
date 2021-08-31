5b63f5bb2bf07b1495e493ba57d1889d0f28cd69
JohnAscenda <john.hilding@ascenda.se>
2021-08-31 16:11:22 +0200

    Add affcode from query to session storage

--- pages/course.tsx
+++ pages/course.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../styles/courses.module.scss";
 import Head from "next/head";
 import Layout from "../components/layout/layout";
-import EmailCourse from "../components/emailForm/forms/emailCourse";
 import FormInput from "../components/emailForm/formInput";
 import FAQ from "../components/faq/faq";
 import questions from "../components/faq/course_faq";
@@ -13,6 +12,7 @@ import PricingCard from "../components/cards/pricingCard/pricingCard";
 import Quote from "../components/quote/quote";
 import info from "../components/cards/infoCard/info";
 import modules from "../components/cards/moduleCard/modules";
+import queryString from "query-string";
 import {
   package_basic,
   package_complete,
@@ -22,7 +22,20 @@ import {
 const form_url =
   "https://gmail.us2.list-manage.com/subscribe/post?u=ff73d806dd2f49da87ede8337&amp;id=ed4e712aca&amp;SIGNUP=FlutterMovieCourse";
 
-const courses = () => {
+const course = () => {
+  const getAffcode = () => {
+    const qs = queryString.parse(window.location.search);
+    const affcode = qs.affcode;
+
+    if (affcode) {
+      window.sessionStorage.setItem("affcode", JSON.stringify(affcode));
+    }
+  };
+
+  useEffect(() => {
+    getAffcode();
+  }, []);
+
   return (
     <Layout>
       <Head>
@@ -57,8 +70,8 @@ const courses = () => {
           <div className="max_width">
             <h1>Build Production Ready Flutter Apps</h1>
             <h2>
-              Learn how to develop high standard Flutter applications. A currated learning session
-              to fast track your skills
+              Learn how to develop high standard Flutter applications. A
+              currated learning session to fast track your skills
             </h2>
             <div className={styles.form}>
               <FormInput
@@ -75,32 +88,36 @@ const courses = () => {
         <section className={styles.introduction}>
           <div className={`max_width ${styles.intro_text}`}>
             <p>
-              Have you used Flutter but wanted to take it to the next level? <br />
-              <br /> Tired of building apps that becomes messy, hard to manage and want a
-              streamlined way of building apps? <br />
+              Have you used Flutter but wanted to take it to the next level?{" "}
+              <br />
+              <br /> Tired of building apps that becomes messy, hard to manage
+              and want a streamlined way of building apps? <br />
               <br />
               <strong>I understand, I’ve been there!</strong>
               <br />
-              <br /> This is not a course where I will go over how to use the basics such as a Row,
-              Column and so on, but instead teach you how to build robust applications from scratch.
+              <br /> This is not a course where I will go over how to use the
+              basics such as a Row, Column and so on, but instead teach you how
+              to build robust applications from scratch.
               <br />
               <br />
-              What I teach in this course is <strong>knowledge</strong> that will take you from
-              building something that just works to something that is <strong>robust</strong> and{" "}
+              What I teach in this course is <strong>knowledge</strong> that
+              will take you from building something that just works to something
+              that is <strong>robust</strong> and{" "}
               <strong>production ready</strong>!
               <br />
-              <br /> Go from more simple concepts such as UI to the more complex layers such as
-              architecture, error handing, testing and much more.
+              <br /> Go from more simple concepts such as UI to the more complex
+              layers such as architecture, error handing, testing and much more.
               <br />
-              <br /> This is primirily targeted towards intermediates BUT if you have a base
-              knowledge of Flutter then it is for you as well!
+              <br /> This is primirily targeted towards intermediates BUT if you
+              have a base knowledge of Flutter then it is for you as well!
               <br />
               <br />
               Do you wish you were rich?
               <br /> Do you wish you were successful?
               <br />
               <strong>
-                This course cant help with that! But it will teach you about state management 😎
+                This course cant help with that! But it will teach you about
+                state management 😎
               </strong>
             </p>
           </div>
@@ -176,18 +193,21 @@ const courses = () => {
                     YouTube channel
                   </a>
                 </strong>{" "}
-                or being a <strong>GDE in Flutter & Dart</strong> but not too long ago I also coded
-                myself in to corners, making code hard to manage and was just lost in how to
-                actually make it scalable.
+                or being a <strong>GDE in Flutter & Dart</strong> but not too
+                long ago I also coded myself in to corners, making code hard to
+                manage and was just lost in how to actually make it scalable.
               </p>
               <p>
-                <strong>I’ve been there but that is also why I know how to get out of it.</strong>{" "}
-                I’ve learned from trial and error, consulting, teaching as well as working with
-                other professionals!
+                <strong>
+                  I’ve been there but that is also why I know how to get out of
+                  it.
+                </strong>{" "}
+                I’ve learned from trial and error, consulting, teaching as well
+                as working with other professionals!
               </p>
               <strong>
-                Save time and frustration and learn an efficient system to build flutter
-                applications
+                Save time and frustration and learn an efficient system to build
+                flutter applications
               </strong>
             </div>
             <img
@@ -226,21 +246,25 @@ const courses = () => {
               <br />
             </h3>
             <p>
-              I made this course as a main goal of giving structure when learning Flutter. Instead
-              of taking <strong>multiple months</strong> or <strong>even years</strong> to get to a
-              solid understanding the course will walk you through that, right away.
+              I made this course as a main goal of giving structure when
+              learning Flutter. Instead of taking{" "}
+              <strong>multiple months</strong> or <strong>even years</strong> to
+              get to a solid understanding the course will walk you through
+              that, right away.
               <br />
-              <br /> Learning things like <strong>Testing, Architecture, Animations</strong> are
-              crucial for building good and reliable apps, and is something that can be very hard to
-              get right.
+              <br /> Learning things like{" "}
+              <strong>Testing, Architecture, Animations</strong> are crucial for
+              building good and reliable apps, and is something that can be very
+              hard to get right.
               <br />
-              <br /> I’ve tought on YouTube with almost <strong>two million views</strong>, done
-              consulting for business and clients. I’ve taken what I teach{" "}
-              <strong>business and clients</strong> in to a course so more people can get something
-              out of it!
+              <br /> I’ve tought on YouTube with almost{" "}
+              <strong>two million views</strong>, done consulting for business
+              and clients. I’ve taken what I teach{" "}
+              <strong>business and clients</strong> in to a course so more
+              people can get something out of it!
               <br />
-              <br /> If I would teach this in a one to one meeting that would be up in the price
-              range of <strong>$2500 or more</strong>.
+              <br /> If I would teach this in a one to one meeting that would be
+              up in the price range of <strong>$2500 or more</strong>.
               <br />
               <br /> The question always comes down to, how serious are you?
             </p>
@@ -283,14 +307,16 @@ const courses = () => {
               100% Satisfaction Guarantee for <strong>30-days</strong>
             </h2>
             <p>
-              After spending so much time on both this course and teaching over 3 years on YouTube
-              and other areas I am confident that this will be worth it.
+              After spending so much time on both this course and teaching over
+              3 years on YouTube and other areas I am confident that this will
+              be worth it.
               <br />
-              <br /> So if you are not happy with the course, for any reason, just reach out to me
-              and I’ll issue a full refund.
+              <br /> So if you are not happy with the course, for any reason,
+              just reach out to me and I’ll issue a full refund.
               <br />
               <br />
-              What is important for me is that you get knowledge that you can apply right away!
+              What is important for me is that you get knowledge that you can
+              apply right away!
             </p>
           </div>
         </section>
@@ -356,4 +382,4 @@ const courses = () => {
   );
 };
 
-export default courses;
+export default course;
