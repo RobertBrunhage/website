582a633e16e4e9b88d4ca8e2773fe7dfa67eb5fa
JohnAscenda <john.hilding@ascenda.se>
2022-01-01 13:40:43 +0100

    Fix course card text

--- components/cards/pricingCard/packages.tsx
+++ components/cards/pricingCard/packages.tsx
@@ -28,7 +28,7 @@ const package_complete: price_package[] = [
     name: "Everything in the basic package",
   },
   {
-    name: "Entire Course 5 modules are currently completed",
+    name: "Entire Course",
   },
   {
     name: "Download all videos for offline access",
@@ -55,7 +55,7 @@ const package_exclusive: price_package[] = [
     name: "2 Hours of 1 on 1 personal teaching",
   },
   {
-    name: "Entire Course 5 modules are currently completed",
+    name: "Entire Course",
   },
   {
     name: "Download all videos for offline access",
