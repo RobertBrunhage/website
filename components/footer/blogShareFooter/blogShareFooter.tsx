4e2420adfe75d797d21585dc31236edaeb356b32
Robert Brunhage <robertbrunhage@gmail.com>
2022-02-09 17:37:14 +0100

    feat: improve twitter button design on blog posts

--- components/footer/blogShareFooter/blogShareFooter.tsx
+++ components/footer/blogShareFooter/blogShareFooter.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import React from "react";
 import styles from "./blogShareFooter.module.scss";
-import ShareSocials from "./shareSocials";
 
 interface BlogShareFooterProps {
   message: String;
@@ -10,7 +9,6 @@ interface BlogShareFooterProps {
 const blogShareFooter = ({ message }: BlogShareFooterProps) => {
   const router = useRouter();
   const generateTwitterUrl = () => {
-    console.log(message);
     const currentUrl = router.asPath;
     const messageWithoutSpace = message.replace(/\s/g, "%20");
     const url = `https://twitter.com/intent/tweet?url=https://robertbrunhage.com${currentUrl}%2F&text=${messageWithoutSpace}`;
@@ -19,20 +17,15 @@ const blogShareFooter = ({ message }: BlogShareFooterProps) => {
 
   return (
     <div className={styles.container}>
-      <h2>Share it on Twitter!</h2>
-      <ul>
-        {ShareSocials.map((item, index) => (
-          <li key={index}>
-            <a
-              href={generateTwitterUrl()}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img src={item.img} alt={item.alt} />
-            </a>
-          </li>
-        ))}
-      </ul>
+      <a href={generateTwitterUrl()} rel="noopener noreferrer" target="_blank">
+        <img
+          src={"/assets/icons/twitter.png"}
+          width={38}
+          height={38}
+          alt={"twitter"}
+        />
+        Share it on Twitter!
+      </a>
     </div>
   );
 };
--- components/headings/h2.tsx
+++ components/headings/h2.tsx
@@ -1,7 +1,6 @@
 import { replaceSpaceWithUnderscore } from "../../core/utils";
 
 const Heading2 = ({ children }: any) => {
-  console.log(children);
   const idText = replaceSpaceWithUnderscore(children);
 
   return <h2 id={idText}>{children}</h2>;
