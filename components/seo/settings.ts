0dcde72b15399579db6a01265be4a2fa5079a040
Robert Brunhage <robertbrunhage@gmail.com>
2022-02-10 22:04:17 +0100

    fix: base url not correct

--- components/seo/settings.ts
+++ components/seo/settings.ts
@@ -1,5 +1,5 @@
 // Create new environment variable for production, preview and use localhost if none of these are set
 
 export const getBaseUrl = () => {
-  return process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000";
+  return process.env.NEXT_PUBLIC_URL;
 };
